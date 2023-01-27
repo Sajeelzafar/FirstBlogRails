e50ee7c6e5757d198417b24ea222499beeda2e9b
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-27 16:11:43 +0500

    Adding models for database data

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+    after_create :increment_comments_counter
+    validates :text, presence: true,
+    validates :author_id, numericality: { only_integer: true },
+    validates :post_id, numericality: { only_integer: true },
+    belongs_to :author, class_name: "User",
+    belongs_to :post
+
+    def increment_comments_counter
+        post.increment!(:comments_counter)
+    end
+end
