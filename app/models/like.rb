552488dbe8ac6604bdc6facd04108407381c9acf
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-27 16:26:15 +0500

    Fix syntax and linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
-    after_create :increment_likes_counter
-    validates :post_id, numericality: { only_integer: true },
-    validates :author_id, numericality: { only_integer: true },
-    belongs_to :author, class_name: "User",
-    belongs_to :post
+  after_create :increment_likes_counter
+  validates :post_id, numericality: { only_integer: true }
+  validates :author_id, numericality: { only_integer: true }
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def increment_likes_counter
-        post.increment!(:likes_counter)
-    end
+  def increment_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
