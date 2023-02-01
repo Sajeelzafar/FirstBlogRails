fae60f4742196dd018b52ec352fb16c5666012df
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-01 15:58:18 +0500

    Verifying response status of urls

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "should have a valid posts page" do
+      get user_posts_path(user_id: 5)
+      expect(response).to have_http_status(200)
+    end
+
+    it "should have a valid post id page" do
+      get user_post_path(user_id: 5, id: 2)
+      expect(response).to have_http_status(200)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "should have a valid users page" do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "should have a valid user_id page" do
+      get user_path(id: 1)
+      expect(response).to have_http_status(200)
+    end
   end
 end
