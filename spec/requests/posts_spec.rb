3ffc0478e5a21692561919c25476635b2a0bed29
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-03 22:13:36 +0500

    Fix rubocop linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,34 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index/:user_id/posts' do
-    it "should have a valid posts page" do
+    it 'should have a valid posts page' do
       get user_posts_path(user_id: 5)
       expect(response).to have_http_status(200)
     end
 
-    it "should render the correct posts page" do
+    it 'should render the correct posts page' do
       get user_posts_path(user_id: 5)
       expect(response).to render_template(:index)
     end
 
-    it "should render the correct response body text in posts page" do
+    it 'should render the correct response body text in posts page' do
       get user_posts_path(user_id: 5)
-      expect(response.body).to include("This is the Posts page")
+      expect(response.body).to include('This is the Posts page')
     end
 
-    it "should have a valid post id page" do
+    it 'should have a valid post id page' do
       get user_post_path(user_id: 5, id: 2)
       expect(response).to have_http_status(200)
     end
 
-    it "should render the correct post_id page" do
+    it 'should render the correct post_id page' do
       get user_post_path(user_id: 5, id: 2)
       expect(response).to render_template(:show)
     end
 
-    it "should render the correct response body text in post_id page" do
+    it 'should render the correct response body text in post_id page' do
       get user_post_path(user_id: 5, id: 2)
-      expect(response.body).to include("This is the Posts page and currently you have selected 2 Post ID.")
+      expect(response.body).to include('This is the Posts page and currently you have selected 2 Post ID.')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,34 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    it "should have a valid users page" do
+    it 'should have a valid users page' do
       get users_path
       expect(response).to have_http_status(200)
     end
 
-    it "should render the correct users page" do
+    it 'should render the correct users page' do
       get users_path
       expect(response).to render_template(:index)
     end
-    
-    it "should render the correct response body text in users page" do
+
+    it 'should render the correct response body text in users page' do
       get users_path
-      expect(response.body).to include("This is the Users page")
+      expect(response.body).to include('This is the Users page')
     end
 
-    it "should have a valid user_id page" do
+    it 'should have a valid user_id page' do
       get user_path(id: 1)
       expect(response).to have_http_status(200)
     end
 
-    it "should render the correct user_id page" do
+    it 'should render the correct user_id page' do
       get user_path(id: 1)
       expect(response).to render_template(:show)
     end
 
-    it "should render the correct response body text in user_id page" do
+    it 'should render the correct response body text in user_id page' do
       get user_path(id: 1)
-      expect(response.body).to include("This is the Users page and currently you have selected 1 user ID.")
+      expect(response.body).to include('This is the Users page and currently you have selected 1 user ID.')
     end
   end
 end
