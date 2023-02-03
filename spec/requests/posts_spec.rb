6e29f76e273441ad0b54b6682b83efcb0695724f
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-03 22:11:03 +0500

    Verify that the response body has correct text

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template(:index)
     end
 
+    it "should render the correct response body text in posts page" do
+      get user_posts_path(user_id: 5)
+      expect(response.body).to include("This is the Posts page")
+    end
+
     it "should have a valid post id page" do
       get user_post_path(user_id: 5, id: 2)
       expect(response).to have_http_status(200)
@@ -20,6 +25,11 @@ RSpec.describe 'Posts', type: :request do
     it "should render the correct post_id page" do
       get user_post_path(user_id: 5, id: 2)
       expect(response).to render_template(:show)
+    end
+
+    it "should render the correct response body text in post_id page" do
+      get user_post_path(user_id: 5, id: 2)
+      expect(response.body).to include("This is the Posts page and currently you have selected 2 Post ID.")
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to render_template(:index)
     end
+    
+    it "should render the correct response body text in users page" do
+      get users_path
+      expect(response.body).to include("This is the Users page")
+    end
 
     it "should have a valid user_id page" do
       get user_path(id: 1)
@@ -20,6 +25,11 @@ RSpec.describe 'Users', type: :request do
     it "should render the correct user_id page" do
       get user_path(id: 1)
       expect(response).to render_template(:show)
+    end
+
+    it "should render the correct response body text in user_id page" do
+      get user_path(id: 1)
+      expect(response.body).to include("This is the Users page and currently you have selected 1 user ID.")
     end
   end
 end
