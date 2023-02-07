e67d98e010f46e7371f9e8e324e98b3ec6135b2c
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-07 21:40:52 +0500

    Adding additional test for linking of posts to post_id page, likes and comments counterin post index page

--- spec/integration_tests/postindex_spec.rb
+++ spec/integration_tests/postindex_spec.rb
@@ -16,6 +16,10 @@ RSpec.describe 'Post Index Page', type: :system do
                       likes_counter: 1)
         @post4 = Post.create(author: @tom, title: 'Final words', text: 'This is my fourth post', comments_counter: 1,
                       likes_counter: 1)
+        @comment1 = Comment.create(post: @post1, author: @tom, text: 'Hi Tom!')
+        @comment2 = Comment.create(post: @post1, author: @tom, text: 'Hi Jerry!')
+        @comment3 = Comment.create(post: @post1, author: @tom, text: 'Hi Toons!')
+
     end
 
     it "should display the username of selected user" do
@@ -50,5 +54,27 @@ RSpec.describe 'Post Index Page', type: :system do
         expect(page).to have_content(@post4.text)
     end
 
+    it "should display the last comments that post has" do
+        visit user_posts_path(@tom.id)
+        expect(page).to have_text(@comment1.text)
+        expect(page).to have_text(@comment2.text)
+        expect(page).to have_text(@comment3.text)
+    end
 
+    it "should display the number of comments that post has" do
+        visit user_posts_path(@tom.id)
+        expect(page).to have_text("Comments: 4")
+        expect(page).to have_text("Comments: 1")
+    end
+
+    it "should display the number of likes that post has" do
+        visit user_posts_path(@tom.id)
+        expect(page).to have_text("Likes: 1")
+    end
+
+    it "should redirect to post's show page when post is clicked" do
+        visit user_posts_path(@tom.id)
+        click_on @post1.title
+        expect(page).to have_current_path user_post_path(user_id: @tom.id, id: @post1.id)
+    end
 end
