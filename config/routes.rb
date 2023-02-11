92f8ee0b27a1e3c3a9a082fe0487921396777b5c
NurkaAmre <nurkerey30@gmail.com>
2023-02-11 15:24:18 +0600

    Migrate add devise to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
+      resources :likes, only: [:create]
+    end
   end
-  resources :posts, only: [:new, :create] do 
-    resources :comments, only: [:new, :create]
-  end
-  resources :likes, only: [:create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/migrate/20230211085047_create_posts.rb
+++ db/migrate/20230211085047_create_posts.rb
@@ -3,9 +3,11 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
-      t.integer :comments_counter
-      t.integer :likes_counter
     end
   end
 end
